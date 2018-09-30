c0fe4407ab2eefc1c9441dabcd0f2b9f3aaf3045
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-10-01 01:38:02 +0800

    Add splash screen Update app icon

---
+++ app/src/main/java/com/systematix/itrack/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.systematix.itrack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
