f724acce291c736c111de105a952722e1614baec
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-11-03 23:45:20 +0800

    Separate send minor reports to its model

--- app/src/main/java/com/systematix/itrack/models/SyncModel.java
+++ app/src/main/java/com/systematix/itrack/models/SyncModel.java
@@ -1,24 +1,13 @@
 package com.systematix.itrack.models;
 
 import android.content.Context;
-import android.util.Log;
 
-import com.android.volley.VolleyError;
 import com.systematix.itrack.components.sync.Sync;
-import com.systematix.itrack.config.UrlsList;
-import com.systematix.itrack.database.AppDatabase;
 import com.systematix.itrack.items.Auth;
-import com.systematix.itrack.items.MinorReport;
 import com.systematix.itrack.items.User;
 import com.systematix.itrack.models.api.GetViolationsApiModel;
-import com.systematix.itrack.utils.Api;
+import com.systematix.itrack.models.api.SendMinorReportsApiModel;
 import com.systematix.itrack.utils.Task;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.List;
 
 public final class SyncModel implements Sync.OnSyncListener {
     @Override
@@ -30,74 +19,7 @@ public final class SyncModel implements Sync.OnSyncListener {
     }
 
     private void syncMinorReports(final Context context) {
-        final AppDatabase db = AppDatabase.getInstance(context);
-
-        final Api.OnApiRespondListener apiRespondListener = new Api.OnApiRespondListener() {
-            @Override
-            public void onApiSuccess(String tag, JSONObject response, boolean success, String msg) {
-                // after sending the request,
-                // empty the minor reports!
-                if (!success) {
-                    return;
-                }
-                Log.i("devtag", "syncModel@Synced!");
-                new Task<>(new Task.OnTaskExecuteListener<Void>() {
-                    @Override
-                    public Void execute() {
-                        db.minorReportDao().deleteAll();
-                        return null;
-                    }
-                }).execute();
-            }
-
-            @Override
-            public void onApiError(String tag, VolleyError error) {
-                // if error, do it again hehehe
-                syncMinorReports(context);
-            }
-
-            @Override
-            public void onApiException(String tag, JSONException e) {
-                // unlikely to pass here
-            }
-        };
-
-        // get list of minor reports from db
-        // then send api request
-        new Task<>(new Task.OnTaskListener<List<MinorReport>>() {
-            @Override
-            public void preExecute() {
-
-            }
-
-            @Override
-            public List<MinorReport> execute() {
-                // get list of minor reports
-                return db.minorReportDao().getAll();
-            }
-
-            @Override
-            public void finish(List<MinorReport> result) {
-                // only continue on if result has something
-                if (result.isEmpty()) {
-                    return;
-                }
-
-                final JSONArray array = Api.collectionRequest(result);
-                final JSONObject params = new JSONObject();
-                try {
-                    params.put("minorReports", array);
-
-                    Api.post(context)
-                        .setTag("sendMinorViolationBatch")
-                        .setUrl(UrlsList.SEND_MINOR_VIOLATION_BATCH_URL)
-                        .setApiListener(apiRespondListener)
-                        .request(params);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).execute();
+        SendMinorReportsApiModel.send(context);
     }
 
     private void syncViolations(final Context context) {
---
+++ app/src/main/java/com/systematix/itrack/models/api/SendMinorReportsApiModel.java
@@ -0,0 +1,90 @@
+package com.systematix.itrack.models.api;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.VolleyError;
+import com.systematix.itrack.config.UrlsList;
+import com.systematix.itrack.database.AppDatabase;
+import com.systematix.itrack.items.MinorReport;
+import com.systematix.itrack.utils.Api;
+import com.systematix.itrack.utils.Task;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public final class SendMinorReportsApiModel {
+    public static void send(final Context context) {
+        final AppDatabase db = AppDatabase.getInstance(context);
+
+        final Api.OnApiRespondListener apiRespondListener = new Api.OnApiRespondListener() {
+            @Override
+            public void onApiSuccess(String tag, JSONObject response, boolean success, String msg) {
+                // after sending the request,
+                // empty the minor reports!
+                if (!success) {
+                    return;
+                }
+                Log.i("devtag", "syncModel@Synced!");
+                new Task<>(new Task.OnTaskExecuteListener<Void>() {
+                    @Override
+                    public Void execute() {
+                        db.minorReportDao().deleteAll();
+                        return null;
+                    }
+                }).execute();
+            }
+
+            @Override
+            public void onApiError(String tag, VolleyError error) {
+                // if error, do it again hehehe
+                send(context);
+            }
+
+            @Override
+            public void onApiException(String tag, JSONException e) {
+                // unlikely to pass here
+            }
+        };
+
+        // get list of minor reports from db
+        // then send api request
+        new Task<>(new Task.OnTaskListener<List<MinorReport>>() {
+            @Override
+            public void preExecute() {
+
+            }
+
+            @Override
+            public List<MinorReport> execute() {
+                // get list of minor reports
+                return db.minorReportDao().getAll();
+            }
+
+            @Override
+            public void finish(List<MinorReport> result) {
+                // only continue on if result has something
+                if (result.isEmpty()) {
+                    return;
+                }
+
+                final JSONArray array = Api.collectionRequest(result);
+                final JSONObject params = new JSONObject();
+                try {
+                    params.put("minorReports", array);
+
+                    Api.post(context)
+                        .setTag("sendMinorViolationBatch")
+                        .setUrl(UrlsList.SEND_MINOR_VIOLATION_BATCH_URL)
+                        .setApiListener(apiRespondListener)
+                        .request(params);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).execute();
+    }
+}
