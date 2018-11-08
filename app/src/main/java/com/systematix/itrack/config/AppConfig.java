dc54294ef3316547983382218bc9db3bd5065ea1
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-11-08 19:02:32 +0800

    Make production

--- app/src/main/java/com/systematix/itrack/config/AppConfig.java
+++ app/src/main/java/com/systematix/itrack/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.systematix.itrack.config;
 
 public final class AppConfig {
-    static final boolean PRODUCTION = false;
+    static final boolean PRODUCTION = true;
     public static final String TAG = "appTag";
 }
--- app/src/main/java/com/systematix/itrack/config/UrlsList.java
+++ app/src/main/java/com/systematix/itrack/config/UrlsList.java
@@ -2,14 +2,14 @@ package com.systematix.itrack.config;
 
 public final class UrlsList {
     public static final String BASE_URL = AppConfig.PRODUCTION
-            ? "to be set"
+            ? "http://feutechitrack.x10host.com/"
             : "http://192.168.1.5/itrack/";
     public static final String API_URL = BASE_URL + "api";
     // append with 'api...'
-    public static final String LOGIN_URL = API_URL + "login";
+    public static final String LOGIN_URL = API_URL + "Login";
     public static final String UPLOADED_IMAGES_URL = BASE_URL + "images/";
-    public static final String GET_USER_URL = API_URL + "user";
-    public static final String GET_MINOR_VIOLATIONS_URL = API_URL + "violation/index/minor";
-    public static final String SEND_MINOR_VIOLATION_URL = API_URL + "minorviolation/make";
-    public static final String SEND_MINOR_VIOLATION_BATCH_URL = API_URL + "minorviolation/batch";
+    public static final String GET_USER_URL = API_URL + "User";
+    public static final String GET_MINOR_VIOLATIONS_URL = API_URL + "Violation/index/minor";
+    public static final String SEND_MINOR_VIOLATION_URL = API_URL + "MinorViolation/make";
+    public static final String SEND_MINOR_VIOLATION_BATCH_URL = API_URL + "MinorViolation/batch";
 }
