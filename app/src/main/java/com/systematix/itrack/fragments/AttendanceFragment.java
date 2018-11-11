0143d5e2293453e5874f29c1b8634c0d42fbf436
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-11-11 19:08:14 +0800

    Add stuff to attendance fragment

--- app/src/main/java/com/systematix/itrack/fragments/AttendanceFragment.java
+++ app/src/main/java/com/systematix/itrack/fragments/AttendanceFragment.java
@@ -5,14 +5,17 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 import com.systematix.itrack.R;
 import com.systematix.itrack.models.FragmentModel;
 import com.systematix.itrack.interfaces.OnNavItemChangeListener;
 import com.systematix.itrack.models.ProgressTextModel;
+import com.systematix.itrack.models.ViewFlipperModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,7 +23,10 @@ import com.systematix.itrack.models.ProgressTextModel;
 public class AttendanceFragment extends Fragment implements FragmentModel.TitleableFragment, OnNavItemChangeListener {
 
 
+    private ViewFlipperModel viewFlipperModel;
     private ProgressTextModel progressTextModel;
+    private SwipeRefreshLayout emptySwipeLayout;
+    private SwipeRefreshLayout errorSwipeLayout;
 
     public AttendanceFragment() {
         // Required empty public constructor
@@ -32,8 +38,16 @@ public class AttendanceFragment extends Fragment implements FragmentModel.Titlea
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_attendance, container, false);
-        final View vAttendance = view.findViewById(R.id.attendance_view);
+        final ViewFlipper viewFlipper = view.findViewById(R.id.attendance_view_flipper);
+        final View vAttendance = viewFlipper.findViewById(R.id.attendance_view);
+        final View vEmpty = viewFlipper.findViewById(R.id.attendance_empty_state_view);
+        final View vError = viewFlipper.findViewById(R.id.attendance_error_state_view);
 
+        emptySwipeLayout = vEmpty.findViewById(R.id.attendance_empty_swipe_layout);
+        errorSwipeLayout = vError.findViewById(R.id.attendance_error_swipe_layout);
+        // errorSwipeLayout.setRefreshing();
+
+        viewFlipperModel = new ViewFlipperModel(viewFlipper, R.id.attendance_loading_layout);
         progressTextModel = new ProgressTextModel(vAttendance);
 
         return view;
@@ -42,6 +56,7 @@ public class AttendanceFragment extends Fragment implements FragmentModel.Titlea
     @Override
     public void onResume() {
         super.onResume();
+        viewFlipperModel.switchTo(R.id.attendance_view);
         progressTextModel.setProgress(100, 100);
     }
 
