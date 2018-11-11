4878d573d2630068af4c5044281af51476d2ee5a
Jefferson Rylee <rylee.jeff385@gmail.com>
2018-11-11 17:53:32 +0800

    Add progress

--- app/src/main/java/com/systematix/itrack/fragments/AttendanceFragment.java
+++ app/src/main/java/com/systematix/itrack/fragments/AttendanceFragment.java
@@ -3,6 +3,7 @@ package com.systematix.itrack.fragments;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.view.ViewGroup;
 import com.systematix.itrack.R;
 import com.systematix.itrack.models.FragmentModel;
 import com.systematix.itrack.interfaces.OnNavItemChangeListener;
+import com.systematix.itrack.models.ProgressTextModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,16 +20,29 @@ import com.systematix.itrack.interfaces.OnNavItemChangeListener;
 public class AttendanceFragment extends Fragment implements FragmentModel.TitleableFragment, OnNavItemChangeListener {
 
 
+    private ProgressTextModel progressTextModel;
+
     public AttendanceFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_attendance, container, false);
+        final View view = inflater.inflate(R.layout.fragment_attendance, container, false);
+        final View vAttendance = view.findViewById(R.id.attendance_view);
+
+        progressTextModel = new ProgressTextModel(vAttendance);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        progressTextModel.setProgress(100, 100);
     }
 
     // TitleableFragment
---
+++ app/src/main/java/com/systematix/itrack/models/ProgressTextModel.java
@@ -0,0 +1,96 @@
+package com.systematix.itrack.models;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Handler;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.systematix.itrack.R;
+
+public final class ProgressTextModel {
+    private final TextView title;
+    private final TextView subtitle;
+    private final ProgressBar progressBar;
+
+    public ProgressTextModel(View view) {
+        this.title = view.findViewById(R.id.component_progress_text_title);
+        this.subtitle = view.findViewById(R.id.component_progress_text_subtitle);
+        this.progressBar = view.findViewById(R.id.component_progress_text_progress);
+    }
+
+    public void setProgress(final float value, final float total) {
+        setSubtitleValue(total);
+        // some animation
+        for (int i = 0; i <= Math.round(value); i++) {
+            final int currValue = i;
+            final int progress = getProgress(i, total);
+            final Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    title.setText(String.valueOf(currValue));
+                    title.setTextColor(getProgressColor(title.getContext(), progress));
+                    setProgressBar(progress);
+                }
+                // slow down when higher
+            };
+
+            if (i == 0) {
+                runnable.run();
+            } else {
+                new Handler().postDelayed(runnable, (progress * (progress / 8)) + 100);
+            }
+        }
+    }
+
+    private void setProgressBar(int progress) {
+        progressBar.setProgress(progress);
+        // set also color
+        final Drawable drawable = progressBar.getProgressDrawable();
+
+        if (!(drawable instanceof LayerDrawable)) {
+            return;
+        }
+
+        final Drawable shape = ((LayerDrawable) drawable).getDrawable(1);
+        final int color = getProgressColor(progressBar.getContext(), progress);
+        shape.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+    }
+
+    private void setSubtitleValue(float fValue) {
+        final int value = Math.round(fValue);
+        final String str = subtitle.getResources().getString(R.string.attendance_view_subtitle, value);
+        subtitle.setText(str);
+    }
+
+    private int getProgress(float value, float total) {
+        int progress = Math.round(value / total * 100);
+        // if progress is greater than 100 or less than 0 :D
+        progress = progress < 0 ? 0 : progress;
+        progress = progress > 100 ? 100 : progress;
+        return progress;
+    }
+
+    private int getProgressColor(Context context, int progress) {
+        return ContextCompat.getColor(context, getProgressColorRes(progress));
+    }
+
+    @ColorRes private int getProgressColorRes(int progress) {
+        if (progress < 20) {
+            return R.color.colorProgress1;
+        } else if (progress < 40) {
+            return R.color.colorProgress2;
+        } else if (progress < 60) {
+            return R.color.colorProgress3;
+        } else if (progress < 80) {
+            return R.color.colorProgress4;
+        } else {
+            return R.color.colorProgress5;
+        }
+    }
+}
